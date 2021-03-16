5f477baa88d5a18db9f32266d79140c532384256
jbeedle19 <josh.beedle@gmail.com>
2021-03-16 17:02:25 -0400

    photos are displaying correctly

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,7 +11,7 @@ function Nav(props) {
     useEffect(() => {
             document.title = capitalizeFirstLetter(currentCategory.name);
         }, [currentCategory]);
-        
+
     return (
         <header className="flex-row px-1">
             <h2>
